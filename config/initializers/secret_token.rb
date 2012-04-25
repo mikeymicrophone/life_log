92e02896f0cab392007fa2bbe3134e8f1a5d632e
Mike Schwab <mike.schwab@gmail.com>
2012-04-24 21:32:09 -0400

    initial snapshot of generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeLog::Application.config.secret_token = '9aebf88cc9af83e3d77e20ac4d6460b015c17229884ef1b0ddb2ea81c45ab72becec9f3f9c71dbe7190b6e1e0b537449fcce3836fa83ad3679a150cf23224bcc'
